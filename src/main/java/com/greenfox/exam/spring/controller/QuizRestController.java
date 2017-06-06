d244c32da510e528ef075d0b5d7545f44e95d21f
Zsolt Nagy <nagyza@gmail.com>
2017-06-06 12:49:26 +0200

    Prepare endpoint but isn't finished

--- src/main/java/com/greenfox/exam/spring/controller/QuizRestController.java
+++ src/main/java/com/greenfox/exam/spring/controller/QuizRestController.java
@@ -1,9 +1,12 @@
 package com.greenfox.exam.spring.controller;
 
+import com.greenfox.exam.spring.model.AnswerWrapper;
 import com.greenfox.exam.spring.model.QuestionWrapper;
 import com.greenfox.exam.spring.service.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,10 @@ public class QuizRestController {
   @GetMapping(value = "/questions")
   public QuestionWrapper getQuestions() {
     return quiz.getQuestions();
+  }
+
+  @PostMapping(value = "/answers")
+  public void answers(@RequestBody AnswerWrapper answerWrapper) {
+
   }
 }
