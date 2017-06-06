f676c158da9b1637128dfb407f6797dffb2c369c
Zsolt Nagy <nagyza@gmail.com>
2017-06-06 09:34:54 +0200

    Create class for questions

---
+++ src/main/java/com/greenfox/exam/spring/model/Question.java
@@ -0,0 +1,27 @@
+package com.greenfox.exam.spring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String question;
+
+  public Question() {
+  }
+
+  public Question(String question) {
+    this.question = question;
+  }
+}
