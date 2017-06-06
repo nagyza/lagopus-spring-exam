19e6f8700afef90f97e3b9224732a2ffabb403ce
Zsolt Nagy <nagyza@gmail.com>
2017-06-06 11:54:06 +0200

    Refactor: delete unnecessary constructor

--- src/main/java/com/greenfox/exam/spring/model/Question.java
+++ src/main/java/com/greenfox/exam/spring/model/Question.java
@@ -10,9 +10,6 @@ public class Question {
   private Long id;
   private String question;
 
-  public Question() {
-  }
-
   public Question(Long id, String question) {
     this.id = id;
     this.question = question;
