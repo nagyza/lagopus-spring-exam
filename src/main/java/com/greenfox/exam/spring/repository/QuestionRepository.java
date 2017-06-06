6d6015c5eb5839a61aafb8d9662de68010c9732f
Zsolt Nagy <nagyza@gmail.com>
2017-06-06 09:50:59 +0200

    Create CrudRepository interface for questions

---
+++ src/main/java/com/greenfox/exam/spring/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.greenfox.exam.spring.repository;
+
+import com.greenfox.exam.spring.model.Question;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends CrudRepository<Question, Long> {
+}
